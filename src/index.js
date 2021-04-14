323149c02054cd15ff381ebcf6aef0592d70bc08
AlexanderM1927 <52513621+AlexanderM1927@users.noreply.github.com>
2021-04-14 18:55:10 -0500

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Parcial from './parcial'
+/*
+1826937
+John Alexander Martinez
+*/
+ReactDOM.render(<Parcial propNum1={2} propNum2={3} usuario="Alexander Martinez" />, document.getElementById('root'));
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
